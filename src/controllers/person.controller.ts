d9fac93e7bdc8ca82a9a0fd15caf933b03248c3d
paulaandrezza <paulaandrezza25@gmail.com>
2023-06-03 23:44:25 -0300

    add get by id for person

--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { Person, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -6,8 +13,25 @@ const prisma = new PrismaClient();
 @Controller('person')
 export class PersonController {
   @Get()
-  async getPerson(): Promise<Person[]> {
+  async allPerson(): Promise<Person[]> {
     const users = await prisma.person.findMany();
     return users;
+  }
+
+  @Post()
+  async createPerson(@Body() personData: Person): Promise<Person> {
+    const createdPerson = await prisma.person.create({
+      data: personData,
+    });
+
+    return createdPerson;
+  }
+
+  @Get(':id')
+  async getPersonById(@Param('id', ParseIntPipe) id: number): Promise<Person> {
+    const person = await prisma.person.findUnique({
+      where: { idPerson: id },
+    });
+    return person;
   }
 }
