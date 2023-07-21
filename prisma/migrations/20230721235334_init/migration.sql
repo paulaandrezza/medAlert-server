6733b1a89a8b21904daae3f6fbcefa70a626118d
paulaandrezza <paulaandrezza25@gmail.com>
2023-07-21 20:59:28 -0300

    removing medicationSchedule table and add firstSchedule to medication

--- prisma/migrations/20230721235334_init/migration.sql
+++ prisma/migrations/20230721235334_init/migration.sql
@@ -22,16 +22,9 @@ CREATE TABLE "Medication" (
     "dosage" INTEGER NOT NULL,
     "frequency" INTEGER NOT NULL,
     "notes" TEXT,
+    "firstSchedule" DATETIME NOT NULL,
     "personId" INTEGER NOT NULL,
     "medicationTypeId" INTEGER NOT NULL,
     CONSTRAINT "Medication_personId_fkey" FOREIGN KEY ("personId") REFERENCES "Person" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "Medication_medicationTypeId_fkey" FOREIGN KEY ("medicationTypeId") REFERENCES "MedicationType" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
--- CreateTable
-CREATE TABLE "MedicationSchedule" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "time" DATETIME NOT NULL,
-    "medicationId" INTEGER NOT NULL,
-    CONSTRAINT "MedicationSchedule_medicationId_fkey" FOREIGN KEY ("medicationId") REFERENCES "Medication" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
