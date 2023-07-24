520947f8c71fde14bd5c16e91dfe757438306669
paulaandrezza <paulaandrezza25@gmail.com>
2023-07-24 16:12:09 -0300

    add getAllPeopleWithDetails

--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -1,10 +1,26 @@
-import { Controller } from '@nestjs/common';
-import { Person } from '@prisma/client';
+import { Controller, Get } from '@nestjs/common';
+import { Person, PrismaClient } from '@prisma/client';
 import { BaseController } from './base.controller';
+
+const prisma = new PrismaClient();
 
 @Controller('person')
 export class PersonController extends BaseController<Person> {
   constructor() {
     super('person');
+  }
+
+  @Get('details')
+  async getAllPeopleWithDetails(): Promise<Person[]> {
+    const people = await prisma.person.findMany({
+      include: {
+        Medication: {
+          include: {
+            medicationType: true,
+          },
+        },
+      },
+    });
+    return people;
   }
 }
